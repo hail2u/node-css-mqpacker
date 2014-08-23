716e4c687c3fca9f0de71f2be4e9054decae4cdb
Kyo Nagashima <kyo@hail2u.net>
2014-08-23 16:21:04 +0900

    Refactor tests

--- test/css-mqpacker_test.js
+++ test/css-mqpacker_test.js
@@ -7,65 +7,75 @@ var postcss = require('postcss');
 
 var mqpacker = require('../index');
 
-var dirFixtures = path.join(__dirname, 'fixtures');
-var dirExpected = path.join(__dirname, 'expected');
-var input = '';
-var expected = '';
-var opts = {};
-var loadInput = function (name) {
-  return fs.readFileSync(path.join(dirFixtures, name + '.css'), {
-    encoding: 'utf8'
-  });
-};
-var loadExpected = function (name) {
-  return fs.readFileSync(path.join(dirExpected, name + '.css'), {
-    encoding: 'utf8'
-  });
-};
+exports.API = function (test) {
+  test.expect(3);
 
-exports.testPublicInterfaces = function (test) {
-  test.expect(4);
+  var input = '.foo{color:black}';
+  var expected = postcss().process(input).css;
 
-  input = '.foo { color: black; }';
-  expected = postcss.parse(input);
-  test.strictEqual(mqpacker.pack(input).css, expected.toString());
-
-  opts.map = true;
-  test.deepEqual(
-    mqpacker.pack(input, opts).map,
-    expected.toResult(opts).map
+  test.strictEqual(
+    mqpacker.pack(input).css,
+    expected
   );
 
   test.strictEqual(
     postcss().use(mqpacker.postcss).process(input).css,
-    expected.toString()
+    expected
   );
 
   // Old interface
   test.strictEqual(
     postcss().use(mqpacker.processor).process(input).css,
-    expected.toString()
+    expected
   );
 
   test.done();
 };
 
-exports.testRealCSS = function (test) {
-  test.expect(4);
+exports['Option: PostCSS options'] = function (test) {
+  test.expect(2);
 
-  var testCases = [
-    'simple',
-    'multi',
-    'query-order',
-    'between-the-packed-rules'
-  ];
+  var opts = {
+    map: true,
+    from: 'from.css',
+    to: 'to.css'
+  };
+  var input = '.foo{color:black}';
+  var processed = mqpacker.pack(input, opts);
+  var expected = postcss().process(input, opts);
 
-  for (var i = 0, l = testCases.length; i < l; i++) {
-    var testCase = testCases[i];
-    input = loadInput(testCase);
-    expected = loadExpected(testCase);
-    test.strictEqual(mqpacker.pack(input).css, expected);
-  }
+  test.strictEqual(
+    processed.css,
+    expected.css
+  );
+
+  test.deepEqual(
+    processed.map,
+    expected.map
+  );
+
+  test.done();
+};
+
+exports["Real CSS"] = function (test) {
+  var testCases = fs.readdirSync(path.join(__dirname, 'fixtures'));
+  var loadInput = function (file) {
+    file = path.join(__dirname, 'fixtures', file);
+
+    return fs.readFileSync(file, 'utf8');
+  };
+  var loadExpected = function (file) {
+    file = path.join(__dirname, 'expected', file);
+
+    return fs.readFileSync(file, 'utf8');
+  };
+
+  testCases.forEach(function (testCase) {
+    test.strictEqual(
+      mqpacker.pack(loadInput(testCase)).css,
+      loadExpected(testCase)
+    );
+  });
 
   test.done();
 };
