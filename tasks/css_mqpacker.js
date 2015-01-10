0f58a87c91c9487425fa25df900900f656b18b14
Kyo Nagashima <hail2u@gmail.com>
2015-01-10 14:07:41 +0900

    Add Grunt plugin

---
+++ tasks/css_mqpacker.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function (grunt) {
+  var pkg = require('../package.json');
+  pkg.name = pkg.name.replace(/-/g, '_');
+
+  grunt.registerMultiTask(pkg.name, pkg.description, function () {
+    var mqpacker = require('../index');
+
+    var options = this.options({});
+
+    this.files.forEach(function (file) {
+      if (file.src.length !== 1) {
+        grunt.fail.warn('This Grunt plugin does not support multiple source files.');
+      }
+
+      var src = file.src[0];
+      var dest = file.dest;
+
+      if (!grunt.file.exists(src)) {
+        grunt.log.warn('Source file "' + src + '" not found.');
+
+        return;
+      }
+
+      if (options.map) {
+        options.from = src;
+        options.to = dest;
+      }
+
+      var processed = mqpacker.pack(grunt.file.read(src), options);
+      grunt.file.write(dest, processed.css);
+      grunt.log.writeln('File "' + dest + '" created.');
+
+      if (processed.map) {
+        var map = dest + '.map';
+        grunt.file.write(map, processed.map);
+        grunt.log.writeln('File "' + map + '" created.');
+      }
+    });
+  });
+};
